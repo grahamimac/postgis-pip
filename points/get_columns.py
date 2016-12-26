f3cef35e6f6ff55c9c3d68ea54cf291c101e70d0
grahamimac <graham3333@Grahams-MacBook-Pro.local>
2016-12-26 08:36:46 -0500

    Speed Up Input

--- points/get_columns.py
+++ points/get_columns.py
@@ -9,7 +9,7 @@ def subset_data(fname):
 			if c > 0 and line != '\r\n':
 				a = line.split(',')
 				if len(a[5]) > 0 and len(a[6]) > 0:
-					f_out.write(str(c) + ',' + a[5][:14] + ' ' + a[6][:14] + '\n')
+					f_out.write('SRID=4269;POINT(' + a[5][:14] + ' ' + a[6][:14] + ')\n')
 			c += 1
 
 if __name__ == '__main__':
